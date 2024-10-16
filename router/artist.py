3d25a16bdf6bae72035325a8d45654d310bcc057
ADMINion <ilgiz_g93@mail.ru>
2024-10-16 20:20:41 +0300

    add member schema and logic in endpoints

--- router/artist.py
+++ router/artist.py
@@ -3,18 +3,35 @@ import time
 
 from fastapi import APIRouter, Depends, Response, status
 from sqlalchemy import select, insert, func, text
+from sqlalchemy.orm import aliased
 from sqlalchemy.ext.asyncio import AsyncSession
 
 from db.database_model import get_db_session_async
-from models.models import Artist, Country, Release, ReleaseType, Track
-from schemas.artist import ArtistSchema, ArtistSchemaBeforeAppend, ArtistSchemaAfterAppend
+from models.models import Artist, ArtistMember, Member, Country, Release, ReleaseType, Track
+from schemas.artist import (
+    ArtistSchema, ArtistSchemaBeforeInsert, ArtistSchemaAfterInsert, 
+    MemberSchema, MemberSchemaBeforeInsert, MemberSchemaAfterInsert)
 
 router = APIRouter()
 
 @router.get("/", response_model=list[ArtistSchema])
 async def get_artists(session: AsyncSession = Depends(get_db_session_async)) -> Optional[list[ArtistSchema]]:
-    query = select(Artist.id, Artist.name, Artist.origin_date, Artist.is_group, Artist.description, Country.name.label('country')).\
-            join(Country, Country.id == Artist.countries_id).\
+    artist_country: Country = aliased(Country)
+    member_country: Country = aliased(Country)
+    query = select(
+                Artist.id, Artist.name, Artist.origin_date, Artist.is_group, Artist.description, artist_country.name.label('country'),
+                func.array_agg(func.jsonb_build_object(
+                    'id', Member.id, 'stage_name', Member.stage_name, 'born_datetime', func.to_char(Member.born_datetime, 'yyyy-mm-dd'), 
+                    'full_age', func.date_part('year', func.age(Member.born_datetime)), 'country', member_country.name, 
+                    'last_name', Member.last_name, 'first_name', Member.first_name, 'middle_name', Member.middle_name, 
+                    'member_since', func.to_char(ArtistMember.member_since, 'yyyy-mm-dd'), 
+                    'member_until', func.to_char(ArtistMember.member_until, 'yyyy-mm-dd'))
+                ).filter(Member.id.isnot(None)).over(partition_by=Artist.id).label('members')
+            ).distinct().\
+            join(artist_country, artist_country.id == Artist.countries_id).\
+            outerjoin(ArtistMember, ArtistMember.artists_id == Artist.id).\
+            outerjoin(Member, Member.id == ArtistMember.members_id).\
+            outerjoin(member_country, member_country.id == Member.countries_id).\
             order_by(Artist.name.asc())
     data = await session.execute(query)
     result = data.mappings().fetchall()
@@ -22,9 +39,25 @@ async def get_artists(session: AsyncSession = Depends(get_db_session_async)) -> 
         return result
     return Response(status_code=status.HTTP_204_NO_CONTENT)
 
-@router.get("/{id}/members")
-async def get_artist_members(id: int, session: AsyncSession = Depends(get_db_session_async)):
-    ...
+@router.get("/{id}/members", response_model=list[MemberSchema])
+async def get_artist_members(id: int, session: AsyncSession = Depends(get_db_session_async)) -> Optional[list[MemberSchema]]:
+    query = select(
+                Member.id, Member.last_name, Member.first_name, Member.middle_name, Member.stage_name, 
+                func.to_char(Member.born_datetime, 'yyyy-mm-dd').label('born_datetime'),
+                func.date_part('year', func.age(Member.born_datetime)).label('full_age'), Country.name.label('country'), 
+                func.to_char(ArtistMember.member_since, 'yyyy-mm-dd').label('member_since'), 
+                func.to_char(ArtistMember.member_until, 'yyyy-mm-dd').label('member_until')
+            ).\
+            select_from(ArtistMember).\
+            join(Member, Member.id == ArtistMember.members_id).\
+            join(Country, Country.id == Member.countries_id).\
+            where(ArtistMember.artists_id == id).\
+            order_by(ArtistMember.member_since.nulls_last(), Member.stage_name)
+    data = await session.execute(query)
+    result = data.mappings().fetchall()
+    if result:
+        return result
+    return Response(status_code=status.HTTP_204_NO_CONTENT)
 
 @router.get("/{id}/releases")
 async def get_artist_releases(id: int, session: AsyncSession = Depends(get_db_session_async)):
@@ -49,8 +82,8 @@ async def get_artist_releases(id: int, session: AsyncSession = Depends(get_db_se
 async def get_artist_tracks(id: int, session: AsyncSession = Depends(get_db_session_async)):
     ...
 
-@router.post("/append_artist", response_model=ArtistSchemaAfterAppend)
-async def append_artist(body: ArtistSchemaBeforeAppend, session: AsyncSession = Depends(get_db_session_async)) -> ArtistSchemaAfterAppend:
+@router.post("/insert_artist", response_model=ArtistSchemaAfterInsert)
+async def insert_artist(body: ArtistSchemaBeforeInsert, session: AsyncSession = Depends(get_db_session_async)) -> ArtistSchemaAfterInsert:
     body: dict = body.model_dump(exclude_none=True)
     query = insert(Artist).values(**body).returning(Artist)
     data = await session.execute(query)
--- schemas/artist.py
+++ schemas/artist.py
@@ -1,24 +1,48 @@
 from datetime import datetime
-from typing import Optional, List
+from typing import Optional
 from pydantic import BaseModel
 
 class TunedModel(BaseModel):
     class Config:
         from_attributes = True
 
-class ArtistSchemaBeforeAppend(TunedModel):
+class MemberSchemaBeforeInsert(TunedModel):
+    last_name: str
+    first_name: str
+    middle_name: Optional[str] = None
+    stage_name: Optional[str] = None
+    countries_id: int
+    born_datetime: Optional[datetime] = None
+
+class MemberSchemaAfterInsert(TunedModel):
+    id: int
+    last_name: str
+    first_name: str
+    middle_name: Optional[str] = None
+    stage_name: Optional[str] = None
+    born_datetime: Optional[datetime] = None
+
+class MemberSchema(MemberSchemaAfterInsert):
+    born_datetime: Optional[str] = None
+    full_age: Optional[int] = None
+    country: str
+    member_since: Optional[str] = None
+    member_until: Optional[str] = None
+
+class ArtistSchemaBeforeInsert(TunedModel):
     name: str
     countries_id: int
     origin_date: Optional[datetime] = None
     is_group: Optional[bool] = None
     description: Optional[str] = None
 
-class ArtistSchemaAfterAppend(TunedModel):
+class ArtistSchemaAfterInsert(TunedModel):
     id: int
     name: str
     origin_date: Optional[datetime] = None
     is_group: Optional[bool] = None
     description: Optional[str] = None
 
-class ArtistSchema(ArtistSchemaAfterAppend):
+class ArtistSchema(ArtistSchemaAfterInsert):
     country: str
+    members: Optional[list[MemberSchema]] = None
